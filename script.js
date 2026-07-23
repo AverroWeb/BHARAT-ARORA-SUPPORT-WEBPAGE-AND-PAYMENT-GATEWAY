9358cc46f12f6313f0ffd5b8584798ea4418f4f7
Averro <averroweb1825@gmail.com>
2026-07-23 20:05:44 +0530

    updated

--- script.js
+++ script.js
@@ -30,6 +30,20 @@ tl.from(".name-txt h1", {
   stagger: 0.1,
   ease: "power2.out"
 }, "-=0.8")
+.from(".para1", {
+  x: -30,
+  opacity: 0,
+  duration: 1,
+  stagger: 0.1,
+  ease: "power2.out"
+}, "-=1.4")
+.from(".para2", {
+  x: -30,
+  opacity: 0,
+  duration: 1,
+  stagger: 0.1,
+  ease: "power2.out"
+}, "-=0.8")
 .to(".support-txt span", {
   y: 0,
   opacity: 1,
@@ -39,7 +53,7 @@ tl.from(".name-txt h1", {
 }, "<");
 
 gsap.to(".small-head", {
-  x: "-98.5%", 
+  x: "-68.5%", 
   duration: 18,  
   ease: "linear",
   repeat: -1,   
@@ -52,8 +66,8 @@ tl.from(".click", {
 },"<");
 
 document.querySelector('.click').addEventListener('click', function () {
-  this.style.backgroundColor = '#580B41';
-  this.style.color = '#ffd7e6';
+  this.style.backgroundColor = '#911a6d';
+  this.style.color = '#f6dee7';
   window.location.href = "upi://pay?pa=bhr8t2@ptyes&pn=SK%20Ashiq&cu=INR&tn=Payment";
 });
 
